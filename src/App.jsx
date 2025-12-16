d90733dcc4ab335cd242e186d39858e44eed685e
Nihal Shah <nihalshah4@gmail.com>
2025-12-16 14:24:18 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import CanadaPR from "./pages/CanadaPR.jsx";
 import AustraliaPR from "./pages/AustraliaPR.jsx";
